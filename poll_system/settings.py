1bcb70ad5900409513ef565de0666c51fcfcc0d9
Evarmedia <mishakmanuel@gmail.com>
2025-03-10 00:19:55 +0100

    chore: modified allowed hosts for deplolyment

--- poll_system/settings.py
+++ poll_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zr70+2azdwk)c012@segp()wuql28xs2z*#82q7g#8^0uw5cp(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
